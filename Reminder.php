3fe5a3f9f917169a9e46971510226de153ee9973
Damien Regad <dregad@mantisbt.org>
2016-08-31 19:36:59 +0200

    Bump version to 2.0 for Mantis 1.3 support

--- Reminder.php
+++ Reminder.php
@@ -4,7 +4,7 @@ class ReminderPlugin extends MantisPlugin {
 	function register() {
 		$this->name        = 'Reminder';
 		$this->description = lang_get( 'reminder_plugin_desc' );
-		$this->version     = '1.23';
+		$this->version     = '2.0';
 		$this->requires    = array('MantisCore'       => '1.3.0',);
 		$this->author      = 'Cas Nuy';
 		$this->contact     = 'Cas-at-nuy.info';
