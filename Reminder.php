7064d4b1b8599693ee7b6912086bc31af6d6233b
CasN <cas@nuy.info>
2023-03-16 16:22:34 +0100

    Update Reminder.php

--- Reminder.php
+++ Reminder.php
@@ -8,7 +8,7 @@ class ReminderPlugin extends MantisPlugin {
 		$this->requires    = array('MantisCore'       => '2.0.0',);
 		$this->author      = 'Cas Nuy';
 		$this->contact     = 'Cas-at-nuy.info';
-		$this->url         = 'http://www.nuy.info';
+		$this->url         = 'https://github.com/mantisbt-plugins/Reminder';
 		$this->page        = 'config';
 	}
 
