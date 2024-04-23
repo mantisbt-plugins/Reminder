01b5fd2cd1d180298777ece162aed6e2ced596a1
Cas <cas@nuy.info>
2024-04-23 10:00:59 +0200

    Update bug_due_overview2.php

--- scripts/bug_due_overview2.php
+++ scripts/bug_due_overview2.php
@@ -44,7 +44,9 @@ if (ON==$t_rem_include){
 		$query .= " and bugs.project_id IN ". $t_rem_projects;
 	}
 }else{
-	$query .= " and bugs.project_id NOT IN ".$t_rem_projects;
+	if (!empty( config_get( 'plugin_Reminder_reminder_project_id' ) )) {
+		$query .= " and bugs.project_id NOT IN ".$t_rem_projects;
+	}
 }
 
 $results = db_query( $query );
